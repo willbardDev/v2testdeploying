4fc39287d49cc84831b1ddd1f56b6e5dd08baedf
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2025-04-20 08:46:48 +0300

    successfull setting organization list v2

--- @jumbo/components/JumboList/components/JumboListToolbar/ToolbarAction.tsx
+++ @jumbo/components/JumboList/components/JumboListToolbar/ToolbarAction.tsx
@@ -9,14 +9,14 @@ interface ToolbarActionProps {
 
 const ToolbarAction: React.FC<ToolbarActionProps> = ({ action, actionTail }) => {
   return (
-    <Grid container justifyContent="flex-end">
+    <Grid container spacing={1} justifyContent="flex-end">
       {action && (
-        <Grid size={12} sx={{ padding: 1 }}>
+        <Grid size={{xs: 12, md: 12}} sx={{ padding: 1 }}>
           {action}
         </Grid>
       )}
       {actionTail && (
-        <Grid size={{xs:12, md: 6}} sx={{ padding: 1 }}>
+        <Grid size={{xs: 12, md: 12}} sx={{ padding: 1 }}>
           {actionTail}
         </Grid>
       )}
