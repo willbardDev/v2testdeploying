60ae0bd0f4ef3d644c58e5fa09c47c33924d383e
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2025-05-18 08:59:31 +0300

    pagination new

--- @jumbo/components/JumboList/components/JumboListPagination/JumboListPagination.tsx
+++ @jumbo/components/JumboList/components/JumboListPagination/JumboListPagination.tsx
@@ -1,50 +1,86 @@
 import React from 'react';
-import { Stack, TablePagination } from '@mui/material';
+import { Stack, Pagination, PaginationItem, Tooltip } from '@mui/material';
 import useJumboList, { JumboListContextType } from '../../hooks/useJumboList';
 import { useDictionary } from '@/app/[lang]/contexts/DictionaryContext';
 
 interface JumboListPaginationProps {
     hidePagination?: boolean;
-    hideItemsPerPage?: boolean;
 }
 
 const JumboListPagination: React.FC<JumboListPaginationProps> = ({
     hidePagination = false,
-    hideItemsPerPage = false,
 }) => {
-    const dictionary = useDictionary();
-
     const {
         activePage,
         itemsPerPage,
         totalCount,
         isLoading,
         setActivePage,
-        setItemsPerPage,
-        itemsPerPageOptions,
         data,
     } = useJumboList() as JumboListContextType;
+    const dictionary = useDictionary();
+
+    const totalPages = Math.ceil(totalCount / itemsPerPage);
+
+    // Default pagination translations structure
+    const paginationTranslations = dictionary.rqList?.pagination || {
+        first: 'Go to first page',
+        last: 'Go to last page',
+        previous: 'Previous page',
+        next: 'Next page',
+        page: 'Go to page {page}',
+        ellipsis: 'More pages'
+    };
+
+    const getTooltipTitle = (item: {
+        type: string;
+        page: number | null;
+    }): string => {
+        switch (item.type) {
+            case 'first':
+                return paginationTranslations.first;
+            case 'last':
+                return paginationTranslations.last;
+            case 'previous':
+                return paginationTranslations.previous;
+            case 'next':
+                return paginationTranslations.next;
+            case 'page':
+                return item.page 
+                    ? paginationTranslations.page.replace('{page}', item.page.toString())
+                    : '';
+            case 'start-ellipsis':
+            case 'end-ellipsis':
+                return paginationTranslations.ellipsis;
+            default:
+                return '';
+        }
+    };
 
     return (
-        <Stack p={1}>
-            {
-                !hidePagination && !(data?.length <= 0 && isLoading) && (
-                    <TablePagination
-                        component="div"
-                        count={totalCount}
-                        labelRowsPerPage={dictionary.rqList.rows}
-                        page={activePage}
-                        onPageChange={(_, nextPageNumber) => {
-                            setActivePage(nextPageNumber);
-                        }}
-                        rowsPerPage={itemsPerPage}
-                        onRowsPerPageChange={(event) => {
-                            setItemsPerPage(Number(event.target.value));
-                        }}
-                        rowsPerPageOptions={hideItemsPerPage ? [] : itemsPerPageOptions}
-                    />
-                )
-            }
+        <Stack 
+            direction="row"
+            justifyContent="flex-end"
+            p={1}
+            width="100%" 
+        >
+            {!hidePagination && !(data?.length <= 0 && isLoading) && (
+                <Pagination
+                    count={totalPages}
+                    page={activePage + 1}
+                    onChange={(_, page) => setActivePage(page - 1)}
+                    shape="rounded"
+                    showFirstButton
+                    showLastButton
+                    siblingCount={1}
+                    boundaryCount={1}
+                    renderItem={(item) => (
+                        <Tooltip title={getTooltipTitle(item)} arrow>
+                            <PaginationItem {...item} />
+                        </Tooltip>
+                    )}
+                />
+            )}
         </Stack>
     );
 };
