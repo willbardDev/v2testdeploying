ea469a62af893c87075c5258091e24413e24e458
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2025-06-15 15:12:22 +0300

    shift costcenter api inside accountAndFinance folder

--- src/components/masters/costCenters/cost-center-services.js
+++ src/components/masters/costCenters/cost-center-services.js
@@ -4,37 +4,37 @@ const costCenterservices = {};
 
 costCenterservices.getList = async (params = {}) => {
     const { page = 1, limit = 10, ...queryParams } = params;
-    const { data } = await axios.get("/api/masters/cost-centers", {
+    const { data } = await axios.get("/api/accountsAndFinance/cost-centers", {
       params: { page, limit, ...queryParams }
     });
     return data;
 },
 
 costCenterservices.getCostCenters = async(response) => {
-    const {data} = await axios.get(`/api/masters/cost-centers/all-cost-centers`)
+    const {data} = await axios.get(`/api/accountsAndFinance/cost-centers/all-cost-centers`)
     return data;
 }
 
 costCenterservices.costCenterDetails = async(id) => {
-    const {data} = await axios.get(`/api/masters/cost-centers/${id}/costCenterDetails`);
+    const {data} = await axios.get(`/api/accountsAndFinance/cost-centers/${id}/costCenterDetails`);
     return data;
 }
 
 costCenterservices.getAllOutlets = async(response) => {
-    const {data} = await axios.get(`/api/masters/cost-centers/getAllOutlets`)
+    const {data} = await axios.get(`/api/accountsAndFinance/cost-centers/getAllOutlets`)
     return data;
 }
 
 costCenterservices.add = async(costCenter) => {
     return await axios.get('/sanctum/csrf-cookie').then(async (response) => {
-        const {data} = await axios.post(`/api/masters/cost-centers/add`,costCenter)
+        const {data} = await axios.post(`/api/accountsAndFinance/cost-centers/add`,costCenter)
         return data;
     })
 }
 
 costCenterservices.update = async(costCenter) => {
     return await axios.get('/sanctum/csrf-cookie').then(async (response) => {
-        const {data} = await axios.put(`/api/masters/cost-centers/${costCenter.id}/update`,costCenter)
+        const {data} = await axios.put(`/api/accountsAndFinance/cost-centers/${costCenter.id}/update`,costCenter)
         return data;    
     })
 }
