09cce9ca543ca255846cadb15266f2b10a97e217
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2025-08-03 19:56:42 +0300

    adapty v1 changes v6

--- @jumbo/components/JumboVerticalNavbar/JumboNavItem.js
+++ @jumbo/components/JumboVerticalNavbar/JumboNavItem.js
@@ -1,7 +1,7 @@
 'use client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import ListItemButton from "@mui/material/ListItemButton";
 import { ListItemIcon, ListItemText } from "@mui/material";
 import CircleIcon from '@mui/icons-material/Circle';
@@ -9,6 +9,7 @@ import { useTranslation } from '@/hooks/useTranslation';
 import { SIDEBAR_VIEWS } from '@jumbo/utilities/constants';
 import { deviceType } from '@/utilities/helpers/user-agent-helpers';
 import { useJumboLayout } from '../JumboLayout/hooks';
+import { BackdropSpinner } from '@/shared/ProgressIndicators/BackdropSpinner';
 
 const menuBefore = {
   left: 0,
@@ -23,9 +24,11 @@ const menuBefore = {
 
 const JumboNavItem = ({ item, isNested, translate }) => {
   const pathname = usePathname();
+  const router = useRouter();
   const { t } = useTranslation();
   const { sidebarOptions, setSidebarOptions } = useJumboLayout();
   const isMobile = deviceType() === 'mobile';
+  const [isLoading, setIsLoading] = useState(false);
 
   const isMiniAndClosed = React.useMemo(() => {
     return sidebarOptions?.view === SIDEBAR_VIEWS.MINI && !sidebarOptions?.open;
@@ -35,78 +38,101 @@ const JumboNavItem = ({ item, isNested, translate }) => {
     return translate ? t(item.label) : item.label;
   }, [item, translate, t]);
 
+  const handleClick = async (e) => {
+    if (pathname !== item.uri) {
+      e.preventDefault();
+      setIsLoading(true);
+      router.push(item.uri);
+    } else if (isMobile) {
+      setSidebarOptions({ open: false });
+    }
+  };
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [pathname]);
+
   if (!item) return null;
 
   return (
-    <ListItemButton
-      component={"li"}
-      sx={{
-        p: 0,
-        overflow: 'hidden',
-        borderRadius: isMiniAndClosed ? '50%' : '0 24px 24px 0',
-        margin: isMiniAndClosed ? '0 auto' : '0',
-        ...(isMiniAndClosed ? { width: 40, height: 40, justifyContent: 'center' } : {}),
-        ...(!isMiniAndClosed ? { '&::before': menuBefore } : {}),
-        '&:hover': {
+    <>
+      <ListItemButton
+        component={"li"}
+        onClick={handleClick}
+        sx={{
+          p: 0,
+          overflow: 'hidden',
+          borderRadius: isMiniAndClosed ? '50%' : '0 24px 24px 0',
+          margin: isMiniAndClosed ? '0 auto' : '0',
+          ...(isMiniAndClosed ? { width: 40, height: 40, justifyContent: 'center' } : {}),
+          ...(!isMiniAndClosed ? { '&::before': menuBefore } : {}),
+          '&:hover': {
             color: theme => theme.jumboComponents.JumboNavbar.nav.action?.hover,
             backgroundColor: theme => theme.jumboComponents.JumboNavbar.nav.background.hover,
-          ...(!isMiniAndClosed ? {
-            '&::before': {
-              ...menuBefore,
-              backgroundColor: theme => theme.jumboComponents.JumboNavbar.nav.tick.hover,
-            }
-          } : {})
-        },
-        ...(pathname === item.uri ? {
-          color: theme => theme.jumboComponents.JumboNavbar.nav.action?.active,
-          backgroundColor: theme => theme.jumboComponents.JumboNavbar.nav.background.active,
-          ...(!isMiniAndClosed ? {
-            '&::before': {
-              ...menuBefore,
-              backgroundColor: theme => theme.jumboComponents.JumboNavbar.nav.tick.active,
-            }
-          } : {})
-        } : {}),
-      }}
-    >
-      <Link
-        href={item.uri}
-        onClick={() => setSidebarOptions({ open: !isMobile })}
-        target={item.target}
-        style={{
-          flex: 1,
-          display: 'flex',
-          alignItems: 'center',
-          overflow: 'hidden',
-          position: 'relative',
-          color: 'inherit',
-          padding: isMiniAndClosed ? 0 : '8px 30px',
-          textDecoration: 'none',
-          justifyContent: isMiniAndClosed ? 'center' : 'flex-start',
+            ...(!isMiniAndClosed ? {
+              '&::before': {
+                ...menuBefore,
+                backgroundColor: theme => theme.jumboComponents.JumboNavbar.nav.tick.hover,
+              }
+            } : {})
+          },
+          ...(pathname === item.uri ? {
+            color: theme => theme.jumboComponents.JumboNavbar.nav.action?.active,
+            backgroundColor: theme => theme.jumboComponents.JumboNavbar.nav.background.active,
+            ...(!isMiniAndClosed ? {
+              '&::before': {
+                ...menuBefore,
+                backgroundColor: theme => theme.jumboComponents.JumboNavbar.nav.tick.active,
+              }
+            } : {})
+          } : {}),
+          ...(isLoading && {
+            opacity: 0.7,
+            pointerEvents: 'none',
+          }),
         }}
       >
-        <ListItemIcon sx={{ minWidth: isMiniAndClosed ? 20 : 32, color: 'inherit' }}>
-          {isNested ? (
-            <CircleIcon sx={{ fontSize: 6, ml: 1 }} />
-          ) : (
-            item.icon
+        <Link
+          href={item.uri}
+          onClick={handleClick}
+          target={item.target}
+          style={{
+            flex: 1,
+            display: 'flex',
+            alignItems: 'center',
+            overflow: 'hidden',
+            position: 'relative',
+            color: 'inherit',
+            padding: isMiniAndClosed ? 0 : '8px 30px',
+            textDecoration: 'none',
+            justifyContent: isMiniAndClosed ? 'center' : 'flex-start',
+          }}
+          aria-disabled={isLoading}
+        >
+          <ListItemIcon sx={{ minWidth: isMiniAndClosed ? 20 : 32, color: 'inherit' }}>
+            {isNested ? (
+              <CircleIcon sx={{ fontSize: 6, ml: 1 }} />
+            ) : (
+              item.icon
+            )}
+          </ListItemIcon>
+          {!isMiniAndClosed && (
+            <ListItemText
+              primary={label}
+              sx={{
+                m: 0,
+                '& .MuiTypography-root': {
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                }
+              }}
+            />
           )}
-        </ListItemIcon>
-        {!isMiniAndClosed && (
-          <ListItemText
-            primary={label}
-            sx={{
-              m: 0,
-              '& .MuiTypography-root': {
-                whiteSpace: 'nowrap',
-                overflow: 'hidden',
-                textOverflow: 'ellipsis',
-              }
-            }}
-          />
-        )}
-      </Link>
-    </ListItemButton>
+        </Link>
+      </ListItemButton>
+      {isLoading && <BackdropSpinner isRouterTransfer />}
+    </>
   );
 };
 
