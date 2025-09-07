1c29c3b38d14b6b975da121cef71042a60e50578
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2025-09-08 00:00:53 +0300

    .

--- src/app/[lang]/(common)/dashboard/page.tsx
+++ src/app/[lang]/(common)/dashboard/page.tsx
@@ -1,5 +1,5 @@
 export const dynamic = "force-dynamic";
-import Dashboard from '@/components/dashboard/Dashboard'
+import Dashboard from '@/components/Dashboard/Dashboard';
 import React from 'react'
 
 function page() {
